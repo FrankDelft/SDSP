2ccc61d9126c4a73ebfaef70f66e92364e8a6d50
FrankDelft <frank@harraway.za.net>
2023-10-30 11:17:01 +0100

    added STOI to metrics

--- metrics.m
+++ metrics.m
@@ -1,8 +1,11 @@
-function [RMSE, SNR] = metrics(predicted_signal, true_signal)
+function [RMSE, SNR,STOI_metric] = metrics(predicted_signal, true_signal)
     RMSE = rmse(true_signal, predicted_signal);
     SNR = snr(true_signal, predicted_signal-true_signal);
+    STOI_metric= stoi(true_signal, predicted_signal,16000);
     disp('RMSE:')
     disp(RMSE)
     disp('SNR[dB]')
     disp(SNR)
+    disp('STOI:')
+    disp(STOI_metric)
 end
--- working spectral subtraction.m
+++ working spectral subtraction.m
@@ -19,12 +19,13 @@ noisy_speech(N_noise+1:N_clean+N_noise)=clean(1:N_clean)+noise(N_noise+1:N_clean
 L=600;
 win=ones(L,1);
 
-SNR=snr(noisy_speech, noise(1:N_clean+N_noise));
-[spectral_subtract_enhanced,psd_n_est]=spectral_subtraction(noisy_speech,N_noise,L,fs,0.5,win);
-SNR2=snr(spectral_subtract_enhanced, noise(1:N_clean+N_noise)); 
-audiowrite("sdspProj\signals\spectral sub\spec1.wav",real(spectral_subtract_enhanced),fs);
+alpha=2;
+beta=0.1;
+[spectral_subtract_enhanced,psd_n_est]=spectral_subtraction(noisy_speech,N_noise,L,0.5,win, alpha,beta);
 
-%% 
+audiowrite(alpha+"_"+beta+".wav",real(spectral_subtract_enhanced),fs);
+
+metrics(spectral_subtract_enhanced(N_noise+1:end), clean(1:N_clean));
 
 
 % Create a figure
@@ -45,33 +46,27 @@ plot(clean(1:N_clean));
 title('Clean speech');
 
 % Create the fourth subplot
-ifft(sqrt(psd_n_est*L))
+ifft(sqrt(psd_n_est*L));
 subplot(2, 2, 4);  % 2 rows, 2 columns, fourth subplot
 plot(psd_n_est);
 title('Noise removed');
-%[freq,PSD_N]=welsch(noise, L, fs,0.5,win);
-%plot(linspace(0,1,L/2),10*log10(PSD_N(1:L/2)));
-
-%hold on
-%plot(linspace(0,1,L/2),10*log10(psd_n_est(1:L/2)));
 
 
 
 
 
-function [reconstructed_signal, psd_noise_est] = spectral_subtraction(audioData, noise_length_start, L, fs,Overlap,window)
+
+function [reconstructed_signal, psd_noise_est] = spectral_subtraction(audioData, noise_length_start, L,Overlap,window, alpha, beta)
     % Calculate number of segments
     N=length(audioData);
     D=floor(L*(1-Overlap));
     K=floor((N-L+D)/D);
-    N_new=K*D-D+L;
     %calculate energy of the window
     U=sum(window.^2).*1/length(window);
     
     %estimate noise based on starting noise samples
-    [~, psd_noise_est]=welsch(audioData(1:noise_length_start),L,fs,0.5,window);
-    
-
+    psd_noise_est=welch(audioData(1:noise_length_start),L,0.5,window);
+   
     enhancedSpeech=zeros(length(audioData),1);
     % Segment audio data and compute periodograms
     for i = 1:K
@@ -84,31 +79,25 @@ function [reconstructed_signal, psd_noise_est] = spectral_subtraction(audioData,
         psd_speech_frame = (1/U)*(1 / L) .* abs(fft_segment).^2;
         phases_frame = angle(fft_segment);  % Store phase information
 
-        alpha=2;
-        beta=0.018;
+     
     
         spectral_sub = psd_speech_frame-alpha.*psd_noise_est';
         factor_reduction = beta.*(psd_noise_est');
         %remove musical noise
         enhanced_psd = max(spectral_sub,factor_reduction);
 
-        gain=psd_speech_frame./((psd_noise_est').*100+psd_speech_frame); 
-        %enhanced_psd=gain.*psd_speech_frame;
-
         %now to reconstruct the speech signal
         enhanced_frame=ifft(sqrt(enhanced_psd.*(U*L)).*exp(phases_frame*1i));
         enhancedSpeech(start_frame:end_frame)=enhancedSpeech(start_frame:end_frame)+enhanced_frame./window;
-
-  
     end
-    reconstructed_signal=enhancedSpeech;
+    reconstructed_signal=enhancedSpeech*Overlap;
 end
 
 
 
 
-% Periodogram averaging Welsch
-function [freq,PSD] = welsch(audioData,L,fs,Overlap,window)
+% Periodogram averaging Welch
+function PSD = welch(audioData,L,Overlap,window)
     % percentage overlap
     N=length(audioData);
     D=L*(1-Overlap);
@@ -127,8 +116,6 @@ function [freq,PSD] = welsch(audioData,L,fs,Overlap,window)
         P_ave_w(:) = P_ave_w(:) + P_i_hat_w(:, i);
     end
     PSD = P_ave_w .* (1 / K);
-    
-    % Create a frequency vector
-    freq = (0:L - 1) * fs / L;
+
 end
 
